8c514fc4ff4e0e144663071fb68e58e21b340185
Lucàs <vabrelucas@gmail.com>
2022-08-30 21:47:12 +0200

    👋 Init prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Quote = {
+  id: string,
+  author: string,
+  en: string
 }
 
-export default App;
+const getRandomQuote = () => {
+  return fetch('https://programming-quotes-api.herokuapp.com/quotes/random')
+  .then(res => res.json());
+};
+
+export default function App() {
+  const [quote, setQuote] = useState({
+    id: '', author: '', en: '',
+  });
+
+  const handleNewQuote = () => {
+    getRandomQuote().then(json => setQuote(json));
+  };
+
+  // Init quote
+  useEffect(() => {
+    handleNewQuote();
+  }, []);
+
+  return (
+      <div className="App">
+        <div id={'quote-box'}>
+          <p id={'text'}>{quote.en}</p>
+          <p id={'author'}>- by {quote.author}</p>
+          <button id={'new-quote'} onClick={handleNewQuote}>New quote</button>
+          <br/>
+          <a id={'tweet-quote'} target={'_blank'}
+             href={'https://twitter.com/intent/tweet?hashtags=quotes&'
+                 + `text="${escape(quote.en)}"${escape(quote.author)}`}>
+            Tweet this
+          </a>
+        </div>
+      </div>
+  );
+}
